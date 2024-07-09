e2ea841b4fdc388883325a2958b4c899c5e91d3c
Sumedh1810 <sumedh020@gmail.com>
2024-07-09 19:14:28 +0530

    Delete my apis used to fetch data required for the app

--- src/api.js
+++ src/api.js
@@ -1,15 +1,18 @@
-const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+const GEO_API_URL = '--';
 
 const geoApiOptions = {
     method: 'GET',
     headers: {
-        'x-rapidapi-key': '98aba81127msh015409001b95627p11ea4bjsn6dcf3e2efa9e',
-        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+        'x-rapidapi-key': '--',
+        'x-rapidapi-host': '--'
     }
 };
 
 export { GEO_API_URL, geoApiOptions };
 
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_URL = "--";
 
-export const WEATHER_API_KEY = "59ca8f4f4e7267b4cde5fb27583103c4"
+export const WEATHER_API_KEY = "--"
+
+// get your weather api from open https://openweathermap.org/
+// get your cities api from https://rapidapi.com/wirefreethought/api/geodb-cities
